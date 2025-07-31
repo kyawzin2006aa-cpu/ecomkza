0cc1efa6c65c829c62b3e9af0098dae208139693
kyawzin2006aa-cpu <kyawzin2006.aa@gmail.com>
2025-07-31 11:52:55 +0630

    Nay Toe

---
+++ admin/index.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "Hello B111, welcome to ecommerce module";
+
+$batch = 111 ;
+$name = "B111";
+$temp = 33.5;
+echo "<b><em>$name</em></b> is in batch <b>$batch</b> and has a temperature of <b>$temp</b>.";
+?>
